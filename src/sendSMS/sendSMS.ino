f644b2f9b52ecbcc3ce0e70d36b4c669b66d39de
Luis Blázquez Miñambres <40697133+luisblazquezm@users.noreply.github.com>
2019-04-05 17:52:42 +0200

    Going forward

---
+++ src/sendSMS/sendSMS.ino
@@ -0,0 +1,52 @@
+#include <GSM.h>
+
+#define PINNUMBER ""
+#define PHONENUMBER "+34651550473"
+
+// initialize the library instance
+GSM gsmAccess;
+GSM_SMS sms;
+
+void setup() {
+  // initialize serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("SMS Messages Sender");
+
+  // connection state
+  boolean notConnected = true;
+
+Serial.println("Antes");
+    // Start GSM shield
+  // If your SIM has PIN, pass it as a parameter of begin() in quotes
+  while(notConnected)
+  {
+    Serial.println("Aqui");
+    if(gsmAccess.begin("5056")==GSM_READY)
+      notConnected = false;
+    else
+    {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+Serial.println("Despues");
+  delay("10000");
+
+  Serial.println("GSM initialized");
+}
+
+void loop() {
+  //Establecer el numero 
+  Serial.println(PHONENUMBER);
+
+  // send the message
+  sms.beginSMS(PHONENUMBER);
+  sms.print("Hola");
+  sms.endSMS();
+
+  delay(10000);
+}
