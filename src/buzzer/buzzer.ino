fac5d4b061bf169cbf5e68d06e3dac27fe88d15b
Alberto <alberto1280@usal.es>
2019-04-21 19:52:55 +0200

    ewf

--- src/buzzer/buzzer.ino
+++ src/buzzer/buzzer.ino
@@ -1,23 +1,23 @@
-//double noteFreqArrSend[] = {  /* TONO ENVIO */
-//  784, 784, 622, 698, 784, 698, 784 };
 
-//double noteFreqArrSend[] = {  /* INICIO */
-//  165.4064, 130.813, 261.626, 523.251, 1046.50, 2093.00, 4186.01
+//double noteFreqArrSend[] = {  /* StartUp */
+ // 165.4064, 130.813, 261.626, 523.251, 1046.50, 2093.00, 4186.01
+//};
+
+//double noteFreqArrSend[] = {  /* ShutDown --> Delay es 100*/
+ //523.251, 391.995, 329.628, 261.626
 //};
 
 //double noteFreqArrSend[] = {  /* Mario victory */
 //   466, 588, 699, 933, 933, 933, 933, 1047
 //};
 
-
 //double noteFreqArrSend[] = {  /* Jingle bells */
-//   659, 659, 659
+//   392, 262, 311, 349, 392, 262/*, 311, 349, 392, 262, 311, 349, 392, 262, 311 */
 //};
 
-double noteFreqArrSend[] = {  /* Jingle bells */
-   392, 262, 311, 349, 392, 262/*, 311, 349, 392, 262, 311, 349, 392, 262, 311 */
+double noteFreqArrSend[] = {  /* Tetris */
+  330, 1, 494, 1, 660, 1, 590, 660, 494, 494, 523, 1, 440, 1, 494, 1, 392, 392, 440, 370, 1, 392
 };
-
 
 double noteFreqArrRecv[] = {  /* Mario victory */
 49.4, 52.3, 55.4, 58.7, 62.2, 65.9, 69.9, 74, 78.4, 83.1, 88, 93.2,
@@ -27,24 +27,31 @@ double noteFreqArrRecv[] = {  /* Mario victory */
 790, 837, 887, 940, 996, 1050, 1110, 1180, 1250, 1320, 1400, 1490,
 1580, 1670, 1770, 1870, 1990, 2100};
 
+//int noteDurationArrSend[] = { /* Startup */
+ // 100, 100, 100, 100, 100, 100, 100
+//};
+
+//int noteDurationArrSend[] = { /* ShutDown */
+ //100, 100, 100, 200
+//};
+
 //int noteDurationArrSend[] = { /* Mario Victory */
 //   100, 100, 100, 300, 100, 100, 100, 400
 //};
 
 //int noteDurationArrSend[] = { /*Jingle bell */
-//   400, 400, 800
+//   500 ,500, 250, 250, 500, 500/*, 250, 250, 500, 500, 250, 250, 500, 500, 250*/
 //};
 
-
-int noteDurationArrSend[] = { /*Jingle bell */
-   500 ,500, 250, 250, 500, 500/*, 250, 250, 500, 500, 250, 250, 500, 500, 250*/
+int noteDurationArrSend[] = { /*Tetris */
+  150, 40, 159, 40, 150, 49, 150, 150, 100, 100, 150, 40, 150, 40, 150, 40, 100, 100, 150, 150, 40, 150
 };
 
 int noteDurationArrRecv[] = { 
   250
 };
 
-int countTonesSend = 15;
+int countTonesSend = 22;
 int countTonesRecv = 0;
 const int pinBuzzer = 8;
  
